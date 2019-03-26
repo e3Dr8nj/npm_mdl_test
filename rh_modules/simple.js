4ad38262ac1f8267cc5012e3ba03d9cae61ff200
Chaos Adept <38735292+e3Dr8nj@users.noreply.github.com>
2019-03-26 23:35:31 +0300

    Update simple.js

--- rh_modules/simple.js
+++ rh_modules/simple.js
@@ -1,24 +1,27 @@
 //________________________________________TOOLS__________________________________________
-let delay=async(duration)=>{await new Promise(resolve=>setTimeout(resolve,duration))}; //* for delay inside async function, use it instead setTimeout
+let delay=async(duration)=>{await new Promise(resolve=>setTimeout(resolve,duration))}; 
+     //* for delay inside async function, use it instead setTimeout
 let random =(max)=>{ return Math.floor(Math.random()*max);};
 
 
 //_________________PART MANAGER (OPCIONAL)
-exports.RH_IGNORE_TOTAL=true;//add this line to ignore this module 
-exports.RH_IGNORE_COMMANDS=false;//add this line to ignore all commands from this module
-module.exports.RH_BOOTS=false;//add this line to ignore all boots from this module
-module.exports.RH_IGNORE_EVENTS=false;//add this line to ignore all events from this module
-module.exports.RH_IGNORE_EVENTS_PRIMITIVE=false;//add this line to ignore all events_primitive from this module
+//exports.RH_IGNORE_TOTAL=true;//add this line to ignore this module 
+//exports.RH_IGNORE_COMMANDS=true;//add this line to ignore all commands from this module
+//module.exports.RH_BOOTS=true;//add this line to ignore all boots from this module
+//module.exports.RH_IGNORE_EVENTS=true;//add this line to ignore all events from this module
+//module.exports.RH_IGNORE_EVENTS_PRIMITIVE=true;//add this line to ignore all events_primitive from this module
 
 
-
+//___________________________ETERNAL_VARIABLE_PART
+module.exports.e={};
+   e.test="test";
 //_________________________________________BOOTS_PART___________________________________________________
 module.exports.boots = {}; 
 
 module.exports.boots.someBoot={run:async(client)=>{try{
     //code to execut bot on loading
 }catch(err){console.log(err);};}};//
-
+//module.exports.boots.someBoot.RH_IGNORE=true;//add this line to ignore this command
 //...
 //_________________________________________COMMANDS_PART_________________________________________________
 module.exports.commands = {};
@@ -26,7 +29,7 @@ module.exports.commands = {};
 module.exports.commands.someCommand={aliase:'aliase_for_command', run:async(client,message,args)=>{try{
    //code to execut then this command triggered
 }catch(err){console.log(err);};}};//
-
+//module.exports.commands.someCommand.RH_IGNORE=true;//add this line to ignore this command
 // ...
 
 
@@ -36,7 +39,7 @@ module.exports.events={};
 module.exports.events.someEvent={ run:async(client,event_parametrs)=>{try{
     //code to execut then event occurs
 }catch(err){console.log(err);};}};//
-
+//module.exports.events.someEvent.RH_IGNORE=true;//add this line to ignore this event trigger
 // ...
 //_________________________________________EVENTS_PART_END__________________________________________
 
@@ -46,3 +49,5 @@ module.exports.events_primitive={};
 module.exports.events_primitive.SOME_EVENT_NAME={run:async(client,event)=>{try{
       //some code here
 }catch(err){console.log(err);};}};//
+//module.exports.events_primitive.SOME_EVENT_NAME.RH_IGNORE = true;//add this line to ignore this primitive event trigger
+
